361ddf6976ae098e23472da7a3f2dea99d134d4c
achrafedd <achrafeddiba@gmail.com>
2024-02-18 16:19:10 +0100

    task 4: number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    Flask App
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """
+        Hello Route
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+
+    """
+        HBNB Route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """
+        C Route
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', defaults={"text": "is_cool"})
+@app.route('/python/<text>')
+def python(text):
+    """
+        Python Route
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """
+        Number Route
+    """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
